ac74aa29c6fdb69a65c05339afc9c6951ccaabe3
Olumide Oyedipe <olu@Olumides-MacBook-Pro.local>
2017-06-20 22:58:22 -0700

    Using a angular http service

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -3,50 +3,21 @@
  */
 'use strict';
 
-eventsApp.controller('EventController', function ($scope) {
+eventsApp.controller('EventController', function ($scope, eventData, $log) {
     $scope.snippet = '<h2 style="color: red;">hi there</h2>';
     $scope.boolValue = false;
     $scope.myStyle = { color: 'red'};
     $scope.myclass = 'blue';
     $scope.buttonDisabled = true;
     $scope.sortorder = 'name';
-    $scope.event = {
-        name: 'Angular Boot Camp',
-        date: 1359781015626,
-        time: '10:30 am',
-        location: {
-            address: 'Google Headquarters',
-            city: 'Mountain View',
-            province: 'CA'
-        },
-        imageUrl: '/img/angularjs-logo.png',
-        sessions: [
-            {
-                name: 'Directives Masterclass introductory',
-                creatorName: 'Bob Smith',
-                duration: 2,
-                level: 'Advanced',
-                abstract: 'In this session you will learn the ins and outs of directives!',
-                upVoteCount: 0
-            },
-            {
-                name: 'Scopes for fun and profit',
-                creatorName: 'John Doe',
-                duration: 1,
-                level: 'Introductory',
-                abstract: 'This session will take a closer look as scopes. Learn what they do, have some fun also!',
-                upVoteCount: 0
-            },
-            {
-                name: 'Well Behaved Controllers',
-                creatorName: 'Jane Doe',
-                duration: 4,
-                level: 'Intermediate',
-                abstract: 'Controllers are the beginning of everything. Learn how to craft controllers that will win the respect of your friends and neighbors.',
-                upVoteCount: 0
-            }
-        ]
-    }
+    $log.warn(eventData);
+    eventData.getEvent().success(
+        function (event) {
+            $scope.event = event;
+        }
+    ).error(function (data, status, headers, config) {
+        $log.warn(data, status, headers, config);
+    });
 
     $scope.upVoteSession = function(session) {
         session.upVoteCount++;

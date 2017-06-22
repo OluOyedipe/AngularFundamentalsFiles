b05c6e8fc44bf368d922316419e7ddfe6cf0bfc6
Olumide Oyedipe <olu@Olumides-MacBook-Pro.local>
2017-06-21 23:24:33 -0700

    use template since angular can compile the markup for you

--- DemoApp/app/js/directives.js
+++ DemoApp/app/js/directives.js
@@ -2,9 +2,8 @@
 
 eventsApp.directive('mySample', function ($compile) {
     return {
-        link: function (scope, element, attrs, controller) {
-            var markup = "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
-            angular.element(element).html($compile(markup)(scope));
-        }
+        restrict: 'C',
+        template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br/>"
+
     }
 });

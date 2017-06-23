16f5123eff63ff07683e687e5498f84cf1647bb4
Olumide Oyedipe <olu@Olumides-MacBook-Pro.local>
2017-06-22 21:53:15 -0700

    Adding a controller into a directive

---
+++ DemoApp/app/js/directives/gravatar.js
@@ -0,0 +1,20 @@
+/**
+ * Created by olu on 6/22/17.
+ */
+'use strict';
+
+eventsApp.directive('gravatar', function (gravatarUrlBuilder) {
+   return {
+       restrict: 'E',
+       replace: true,
+       template: '<img />',
+       link: function (scope, element, attrs, controller) {
+           attrs.$observe('email', function (newValue, oldValue) {
+               if (newValue !== oldValue) {
+                   attrs.$set('src', gravatarUrlBuilder.buildGravatarUrl(newValue));
+               }
+
+           })
+       }
+   };
+});
---
+++ DemoApp/app/js/directives/greeting.js
@@ -0,0 +1,17 @@
+/**
+ * Created by olu on 6/22/17.
+ */
+'use strict';
+
+eventsApp.directive('greeting', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
+        controller: function ($scope) {
+            $scope.sayHello = function () {
+                alert('Hello');
+            }
+        }
+    };
+});
